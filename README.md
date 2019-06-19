d0cb0c36346fe5d0d964d7a4c31351e8834e4bd2
duddel <duddel@users.noreply.github.com>
2019-06-19 21:05:21 +0200

    1 note added, extended README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # c-notes
 
-Random notes related to **C**, **C++** and **CMake** about stuff I tend to forget.
+Random notes related to **C** and **C++** about stuff I tend to forget.
+
+* [braced_init.cpp](braced_init.cpp) - **C++11** - _braced initializer lists_
+* [struct_vs_class.cpp](struct_vs_class.cpp) - **C++** - _structure vs. class_
+* [variadic.c](variadic.c) - **C** - _variadic functions_
---
+++ variadic.c
@@ -0,0 +1,80 @@
+/*
+this file is part of c-notes
+https://github.com/duddel/c-notes
+
+C - variadic functions
+*/
+
+#include <stdarg.h>
+#include <stdio.h> // printf()
+
+#define SUM_INT(...) sumi(__VA_ARGS__)
+
+int sumi(int count, ...);
+int sumi2(int count, ...);
+int sumi3(int count, ...);
+int vsumi(int count, va_list va);
+
+int main()
+{
+    // a variadic function is a function with variable number of arguments
+    // printf() itself is such a function
+
+    // sumi() is a variadic function
+    printf("sumi(): %i\n", sumi(3, 1, 2, 3));
+
+    // sumi2() forwards the variable arguments to vsumi()
+    printf("sumi2(): %i\n", sumi2(3, 1, 2, 3));
+
+    // sumi3() copies and forwards the variable arguments to vsumi()
+    printf("sumi3(): %i\n", sumi3(3, 1, 2, 3));
+
+    // SUM_INT is a variadic macro
+    printf("SUM_INT(): %i\n", SUM_INT(3, 1, 2, 3));
+
+    return 0;
+}
+
+int sumi(int count, ...)
+{
+    va_list args;
+    va_start(args, count); // access arguments following argument count
+    int sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        /* assuming all arguments are int. printf() parses
+        the format string to determine the argument types. */
+        sum += va_arg(args, int);
+    }
+    va_end(args);
+    return sum;
+}
+
+int sumi2(int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+    vsumi(count, args);
+    va_end(args);
+}
+
+int sumi3(int count, ...)
+{
+    va_list args;
+    va_list args2;
+    va_start(args, count);
+    va_copy(args2, args); // C99
+    vsumi(count, args2);
+    va_end(args); // args unused
+    va_end(args2);
+}
+
+int vsumi(int count, va_list args)
+{
+    int sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        sum += va_arg(args, int);
+    }
+    return sum;
+}
