d82c4fb13d6e4f0e29be32bab83eed997abe70aa
duddel <duddel@users.noreply.github.com>
2020-12-22 22:23:04 +0100

    add typeid

--- README.md
+++ README.md
@@ -15,4 +15,5 @@ Random notes related to **C** and **C++** about stuff I tend to forget.
 -   [template.h](template.h), [_impl.cpp](template_impl.cpp), [_main.cpp](template_main.cpp) - **C++** - _templates and specialization_
 -   [threading.cpp](threading.cpp) - **C++11** - _multithreading in C++11_
 -   [std_valarray.cpp](std_valarray.cpp) - **C++** - _arrays of numeric values_
+-   [typeid.cpp](typeid.cpp) - **C++** - _type information_
 -   [variadic.c](variadic.c) - **C, C++** - _variadic functions_
---
+++ typeid.cpp
@@ -0,0 +1,51 @@
+/*
+Copyright (c) 2020 Alexander Scholz
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
+#include <typeinfo>
+#include <cstdio> // printf()
+
+struct A { };
+typedef A AT;
+struct B : A { };
+namespace Aspace
+{
+    struct A { };
+}
+
+template <class T>
+void printTypeInfo()
+{
+    const std::type_info &tT = typeid(T);
+    std::printf("%s (%u)\n", tT.name(), tT.hash_code());
+}
+
+int main()
+{
+    // the typeid() operator returns type information (std::type_info):
+    //   name: implementation defined name of a type
+    //   hash: since C++11, guaranteed to be identical for same types
+
+    printTypeInfo<A>();
+    printTypeInfo<A &>();       // same as A
+    printTypeInfo<AT>();        // same as A
+    printTypeInfo<A *>();       // pointer
+    printTypeInfo<B>();         // derived type
+    printTypeInfo<Aspace::A>(); // also work for namespaced types
+    printTypeInfo<int>();
+}
